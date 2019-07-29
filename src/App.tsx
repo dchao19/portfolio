e7acb01178d206da62e24dfb9aa184ffe051ac10
Daniel Chao <daniel@danielchao.me>
2019-07-29 03:04:20 +0000

    Add in-construction site

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,9 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hold tight!</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        I'm working on an update. 
       </header>
     </div>
   );

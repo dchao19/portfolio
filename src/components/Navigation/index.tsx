5096b646f286443df2d72639543ec7f63fba6e65
Daniel Chao <daniel@danielchao.me>
2019-08-03 03:18:25 +0000

    Add navigation component

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import NavigationLink from "./NavigationLink";
+
+import "./index.css";
+
+const Navigation = () => (
+    <nav>
+        <ul className="Navigation-list">
+            <NavigationLink to="/" text="About Me" />
+            <NavigationLink to="/projects" text="Projects" />
+            <NavigationLink to="/experience" text="Experience" />
+            <NavigationLink to="/education" text="Education" />
+            <NavigationLink to="/skills" text="Skills" />
+        </ul>
+    </nav>
+);
+
+export default Navigation;
